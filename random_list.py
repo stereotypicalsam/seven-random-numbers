b80c4ef85420ddcfe2a63e275311b97a38d2429b
stereotypicalsam <samuel.larose94@gmail.com>
2022-01-31 17:31:54 -0600

    Create random_list.py
    makes seven random numbers in a list

---
+++ random_list.py
@@ -0,0 +1,6 @@
+import random
+#imports the random module
+
+seven_random_letters = [random.randint(1,10) for item in range(7)]
+#makes a list that has seven random numbers between 1 and ten
+print(seven_random_letters)
